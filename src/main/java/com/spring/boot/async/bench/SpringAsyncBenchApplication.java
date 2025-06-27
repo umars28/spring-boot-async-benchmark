487892efcaa82b67f209db0e59aa5961d4fb130c
Umar <umarsabirin369@gmail.com>
2025-06-28 00:08:07 +0800

    Init project

---
+++ src/main/java/com/spring/boot/async/bench/SpringAsyncBenchApplication.java
@@ -0,0 +1,13 @@
+package com.spring.boot.async.bench;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAsyncBenchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAsyncBenchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/boot/async/bench/SpringAsyncBenchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.boot.async.bench;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAsyncBenchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
